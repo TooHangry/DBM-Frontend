e3a6967d9dbd8e7cf58f9f8f5b16cfec171aaf70
TooHangry <roberteconnolly22@gmail.com>
2021-10-20 18:57:21 -0400

    Login page

--- src/app/pages/homes/components/add-home-modal/add-home-modal.component.ts
+++ src/app/pages/homes/components/add-home-modal/add-home-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { HomeToAdd } from 'src/app/models/home.models';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { NavService } from 'src/app/services/nav-service/nav.service';
 
 @Component({
@@ -12,11 +13,20 @@ export class AddHomeModalComponent implements OnInit {
   emailRegx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   @Output() addHome: EventEmitter<HomeToAdd> = new EventEmitter();
   @Output() cancelModal: EventEmitter<null> = new EventEmitter();
-  constructor(private navService: NavService) { }
+  constructor(private navService: NavService, private authService: AuthService) { }
   emails: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   canSave: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
+  flag = false;
+  userEmail = '';
+  
   ngOnInit(): void {
+    this.authService.user.subscribe(user => {
+      if (!this.flag && user) {
+        this.flag = true;
+        this.userEmail = user.email;
+        this.emails.next([user.email, ... this.emails.value]);
+      }
+    });
   }
 
   save(): void {
@@ -53,7 +63,7 @@ export class AddHomeModalComponent implements OnInit {
 
     if (this.emailRegx.test(email)) {
       if (!this.emails.value.includes(email)) {
-        this.emails.next([...this.emails.value, email]);
+        this.emails.next([email, ...this.emails.value]);
       }
       
       (document.getElementById('home-email') as HTMLInputElement).value = "";
@@ -74,5 +84,9 @@ export class AddHomeModalComponent implements OnInit {
   checkSave(): void {
     const name = (document.getElementById('home-name') as HTMLInputElement).value;
     this.canSave.next(name.length > 0 && this.emails.value.length > 0);
+  }
+
+  notUserEmail(email: string): boolean {
+    return this.userEmail.length > 0 && this.userEmail !== email;
   }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -11,24 +11,34 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
   errorMessage: BehaviorSubject<string> = new BehaviorSubject<string>('');
-
+  isLogin = true;
+  
   ngOnInit(): void {
   }
 
   login(): void {
-    const email = (document.getElementById('email') as HTMLInputElement).value;
-    const password = (document.getElementById('password') as HTMLInputElement).value;
+    const email = (document.getElementById('login-email') as HTMLInputElement).value;
+    const password = (document.getElementById('login-password') as HTMLInputElement).value;
 
     this.authService.login(email, password); //.subscribe((user: User));
   }
 
   signup(): void {
-    const fname = (document.getElementById('first-name') as HTMLInputElement).value;
-    const lname = (document.getElementById('last-name') as HTMLInputElement).value;
+    const fname = (document.getElementById('signup-fname') as HTMLInputElement).value;
+    const lname = (document.getElementById('signup-lname') as HTMLInputElement).value;
     const email = (document.getElementById('signup-email') as HTMLInputElement).value;
-    const password = (document.getElementById('signup-password') as HTMLInputElement).value;
+    const password1 = (document.getElementById('signup-email') as HTMLInputElement).value;
+    const password2 = (document.getElementById('signup-password') as HTMLInputElement).value;
 
-    this.authService.signup(email, password, fname, lname); //.subscribe((user: User));
+
+    if (password1 == password2) {
+      this.authService.signup(email, password1, fname, lname);
+    }
+
+    // this.authService.signup(email, password, fname, lname); //.subscribe((user: User));
   }
 
+  toggleLogin(): void {
+    this.isLogin = !this.isLogin;
+  }
 }
